8916d8d58dfee29ca932d3abb39933164e4f4707
Peter Polidoro <peterpolidoro@gmail.com>
2021-06-25 14:17:37 -0400

    Create AT42QT2120

---
+++ examples/AT42QT2120/AT42QT2120.ino
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <AT42QT2120.h>
+#include <Streaming.h>
+
+
+AT42QT2120 at42qt;
+
+void setup()
+{
+}
+
+void loop()
+{
+}
---
+++ src/AT42QT2120.h
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------
+// AT42QT2120.h
+//
+// Authors:
+// Peter Polidoro peterpolidoro@gmail.com
+// ----------------------------------------------------------------------------
+#ifndef AT42QT2120_H
+#define AT42QT2120_H
+#include <Arduino.h>
+#include <Wire.h>
+#include "AT42QT/AT42QT.h"
+
+
+class AT42QT2120
+{
+public:
+private:
+};
+
+#endif
